232912550c221a0a7cedcbb77cca51d49496675c
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-03 21:47:08 -0400

    added new and create actions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,30 +1,51 @@
 class PicturesController < ApplicationController
 	
   def index
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+    ]
 	end
 
-  def picture0
-    @picture = {
-      :title  => "The old church on the coast of White sea",
-      :artist => "Sergey Ershov",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    }
+  def show
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+    ]
+    @picture = @pictures[params[:id].to_i]
   end
 
-  def picture1
-    @picture = {
-      :title  => "Sea Power",
-      :artist => "Stephen Scullion",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-    }
+  def new
   end
 
-  def picture2
-    @picture = {
-      :title  => "Into the Poppies",
-      :artist => "John Wilhelm",
-      :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-    }
+  def create
+    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
   end
 
 end
